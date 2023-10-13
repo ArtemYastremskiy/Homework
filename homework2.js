b20a44ca6fd6102ebf3f6e711e2db7270e15c2bc
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-13 18:58:12 +0300

    Update

---
+++ homework2.js
@@ -0,0 +1,42 @@
+/*
+1) вивчит методи списків 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+new Array, []
+of
+from
+at
+concat
+every
+fill
+filter
+some
+find
+findIndex
+flat
+forEach
+map
+reduce
+reduceRight
+includes
+indexOf
+isArray
+join
+lastIndexOf
+keys
+reverse
+pop
+push
+shift
+unshift
+slice
+splice
+sort
+toString
+values
+entries
+keys
+
+Task 1 
+text = 'Hello my name Mer old 19, my sister Lisa 24 old' => 19, 24 => 19 + 24 => 43
+console.log(result) // 43
+*/
