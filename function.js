3a4eed6c8aaf6a12ea0b99dc6c767340f37586e5
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-20 18:49:33 +0300

    Create function.js

---
+++ function.js
@@ -0,0 +1,106 @@
+// a + b = c
+/*
+PYTHON
+def sum(a,b)
+    return a + b
+*/
+// JAVASCRIPT
+// function sum(a,b){
+//     return a+b
+// }
+
+// console.log(sum(2,2));
+
+// ES6
+// =>
+
+// const sum = (a,b) => a+b;
+// const eae = sum(2,2)
+// console.log(eae);
+
+// const sum = function(a,b){
+//     return a + b
+// }
+
+// console.log(sum(2,2));
+
+// sum() визвати функцію
+
+//
+
+// (function sum(a,b){
+//     return a + b
+// })
+
+// function sum(a,b = 1){
+//      return a/b
+// }
+// console.log(sum(3,4));
+
+// (() => result)
+
+// a + b = c
+/*
+PYTHON
+def sum(a,b)
+    return a + b
+*/
+// JAVASCRIPT
+// function sum(a,b){
+//     return a+b
+// }
+
+// console.log(sum(2,2));
+
+// ES6
+// =>
+
+// const sum = (a,b) => a+b;
+// const eae = sum(2,2)
+// console.log(eae);
+
+// const sum = function(a,b){
+//     return a + b
+// }
+
+// console.log(sum(2,2));
+
+// sum() визвати функцію
+
+//
+
+// (function sum(a,b){
+//     return a + b
+// })
+
+// function sum(a,b = 1){
+//      return a/b
+// }
+// console.log(sum(3,4));
+
+// (() => result)
+
+// const arr = [1, 2, 3];
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+//const arr = [1,2,3]
+// const resultArr = arr.forEach((elem) => console.log(elem))
+
+// TASK = function => [1,2,3,4,....n]^2 = [1,4,9,16,25....n]
+
+// function SQArrTwo(arr) {
+//     const newArr = [];
+//     arr.forEach((elem) => newArr.push(elem ** 2));
+//     return newArr;
+// }
+// console.log(SQArrTwo([1,2,3,4,5,6,7,8,9]));
+
+// TASK = function => [1,2,3,4,....n]^2 = [1,4,9,16,25....n] => sum
+// function SQArrTwo(arr) {
+//     const newArr = [];
+//     arr.forEach((elem) => newArr.push(elem ** 2));
+//     return newArr.reduce((accumulator, currentValue) =>  accumulator + currentValue,0);
+// }
+// console.log(SQArrTwo([1,2,3,4,5,6,7,8,9]));
