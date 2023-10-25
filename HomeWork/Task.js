65e9944940ec0278a7037596246d305bb67e2665
Artem <artekyas12@gmail.com>
2023-10-25 18:42:18 +0300

    update

--- HomeWork/Task.js
+++ HomeWork/Task.js
@@ -36,3 +36,6 @@ values =>
 entries =>  
 keys =>  
 */
+
+
+
--- Practica/index.js
+++ Practica/index.js
@@ -26,6 +26,27 @@ const obj = {
     friend: {
         name: "Rose",
         age: 25,
-        hobbies: [1, 9, 1, 6, 2, 4],
+        hobbies: []//[1, 9, 1, 6, 2, 4],
     },
 };
+
+
+
+function sum(obj) {
+    try {
+        let sum1 = `Lisa: ${obj.hobbies.reduce((acc, num) => acc + num)}`
+        let sum2 = `Rose: ${obj.friend.hobbies.reduce((acc, num) => acc + num)}`
+
+        return `${sum1}\n${sum2}`
+    }catch (error){
+        return "Помилка"
+    }
+
+}
+
+
+console.log(sum(obj));
+
+
+//console.log(`Lisa: ${obj.hobbies.reduce((acc, num) => acc + num)}`);
+//console.log(`Rose: ${obj.friend.hobbies.reduce((acc, num) => acc + num)}`);
