4d5ee9d1b025b415c338ac7aab0f811940d3efb3
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2023-10-25 18:29:09 +0300

    Update index.js

--- Practica/index.js
+++ Practica/index.js
@@ -1,4 +1,4 @@
-// n перевірити що ця змінна створена  if 
+// n перевірити що ця змінна створена  if
 // const n = false;
 // if(!n){
 //     console.log('Такого не має');
@@ -6,12 +6,26 @@
 //     console.log('Така є');
 // }
 
-
 // n перевірити що ця змінна створена  try
 
 // //const n = 1
 // try {
 //     console.log(n);
 // } catch (error) {
-//     console.log('Сталась помилка');   
+//     console.log('Сталась помилка');
 // }
+
+// Отримати суму hobbies у Lisa та Rose та повернути результат 
+// Lisa => 66
+// Rose => 15
+
+const obj = {
+    name: "Lisa",
+    age: 25,
+    hobbies: [1, 9, 1, 6, 2, 5, 2, 7, 9, 1, 10, 4],
+    friend: {
+        name: "Rose",
+        age: 25,
+        hobbies: [1, 9, 1, 6, 2, 4],
+    },
+};
